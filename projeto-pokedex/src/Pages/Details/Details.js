3b8d65bf727fabfba08be12878901e8843c240c6
savio henriques mourao ayres <savioayres@hotmail.com>
2022-07-19 11:20:41 -0300

    Inciando o projeto Pokédex

--- projeto-pokedex/src/Pages/Details/Details.js
+++ projeto-pokedex/src/Pages/Details/Details.js
@@ -1,8 +1,44 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import { goToPage } from '../../Routes/Coordinator'
+import Logo from '../../assets/images/Logo.png'
+import { ChakraProvider } from '@chakra-ui/react'
+import { Button, ButtonGroup } from '@chakra-ui/react'
+
+import {
+  DivContainerDetails,
+  GlobalStyle,
+  Header,
+  LogoImage,
+  Main,
+  DivButtonBack
+} from '../../Style/PageDetailStyled'
 
 function Details() {
+  const Navigate = useNavigate()
+
   return (
-    <div>Details</div>
+    <ChakraProvider>
+      <DivContainerDetails>
+        <GlobalStyle />
+        <Header>
+          <DivButtonBack>
+            <Button
+              onClick={() => goToPage(Navigate, 'pokedex')}
+              colorScheme={'twitter'}
+              w={'12vw'}
+            >
+              Voltar
+            </Button>
+          </DivButtonBack>
+          <LogoImage src={Logo} alt="logo"></LogoImage>
+        </Header>
+
+        <Main>
+          <h1>Details</h1>
+        </Main>
+      </DivContainerDetails>
+    </ChakraProvider>
   )
 }
 
--- projeto-pokedex/src/Routes/Routes.js
+++ projeto-pokedex/src/Routes/Routes.js
@@ -1,20 +1,19 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Intro from '../Pages/Intro.js/Intro'
 import Home from './../Pages/Home/Home'
 import Pokedex from './../Pages/Pokedex/Pokedex'
 import Details from './../Pages/Details/Details'
 
-
-export const Router = () =>{
-
-return(
-
+export const Router = () => {
+  return (
     <BrowserRouter>
-    <Routes>
-    <Route path ='/home' index element={<Home/>}/>
-    <Route index element={<Home/>}/>
-    <Route path='/pokedex' element={<Pokedex/>}/>
-    <Route path='/details' element={<Details/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/intro" index element={<Intro />} />
+        <Route index element={<Intro />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+        <Route path="/details" element={<Details />} />
+      </Routes>
     </BrowserRouter>
-)
+  )
 }
