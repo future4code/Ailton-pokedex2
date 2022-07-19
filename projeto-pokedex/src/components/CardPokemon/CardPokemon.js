678b361a8b040a9603eb65a0c0f79b1939363643
savio henriques mourao ayres <savioayres@hotmail.com>
2022-07-19 16:47:21 -0300

    Tentativa de implementação da api

--- projeto-pokedex/src/components/CardPokemon/CardPokemon.js
+++ projeto-pokedex/src/components/CardPokemon/CardPokemon.js
@@ -1,25 +1,16 @@
 import React from 'react'
-import axios from "axios"
+import axios from 'axios'
 import { useContext, useState, useEffect } from 'react'
 import { Context } from '../../Constants/createContext'
-import {dadosPokemon} from '../../Hooks/useRequestData'
-
-
+import { dadosPokemon } from '../../Hooks/useRequestData'
 
 export default function CardPokemon() {
   const [pokemon, setPokemon] = useState({})
-  const ctx = useContext(Context);
-
-
-
-  // const dadosCard = dadosPokemon.filter((pokemon) => {
-  //   for
-  // })
-  
-
-
-
-  return (
-    <div>Card Pokemon</div>
+  const [dadosPokemon] = useRequestData(
+    [],
+    'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0'
   )
+  const ctx = useContext(Context)
+
+  return <div>Card Pokemon</div>
 }
