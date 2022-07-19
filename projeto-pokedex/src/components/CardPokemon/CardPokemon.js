c8e6ee02802a0548b0407d997748f6028fcc2186
Raoni Lobo <teste@MBP-de-Raoni.bbrouter>
2022-07-19 17:57:15 -0300

    funcao

--- projeto-pokedex/src/components/CardPokemon/CardPokemon.js
+++ projeto-pokedex/src/components/CardPokemon/CardPokemon.js
@@ -2,14 +2,11 @@ import React from 'react'
 import axios from 'axios'
 import { useContext, useState, useEffect } from 'react'
 import { Context } from '../../Constants/createContext'
-import { dadosPokemon } from '../../Hooks/useRequestData'
+import { GetPokemons} from '../../Hooks/useRequestData'
 
 export default function CardPokemon() {
   const [pokemon, setPokemon] = useState({})
-  const [dadosPokemon] = useRequestData(
-    [],
-    'https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0'
-  )
+  const [dadosPokemon] = GetPokemons ('https://pokeapi.co/api/v2/pokemon/?limit=20&offset=0', {})
   const ctx = useContext(Context)
 
   return <div>Card Pokemon</div>
