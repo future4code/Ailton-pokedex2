aaa90f31907378094b96d56134e2c8aa9d3c3027
Lincoln Ribeiro de Paula <lincoln20012@outlook.com>
2022-07-19 14:37:32 -0300

    card-home

--- projeto-pokedex/src/App.js
+++ projeto-pokedex/src/App.js
@@ -1,15 +1,25 @@
 import {Router} from './Routes/Routes'
+import {BASE_URL} from './Constants/Url'
+import {GetPokemons} from './Hooks/useRequestData'
+import {Context} from './Constants/createContext'
 
+function App() {  
 
-function App() {
+  const [data, isLoading, error] = GetPokemons(`${BASE_URL}`);
+  const Provider = Context.Provider;
+  // console.log(Provider)
 
-
+  const value ={
+    pokemons: data,
+  }
 
   return (
 
     <div>
+      <Provider value={value}>
 
       <Router></Router>
+      </Provider>
       
     </div>
   );
--- projeto-pokedex/src/Style/IntroStyled.js
+++ projeto-pokedex/src/Style/IntroStyled.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
-
+import VT323 from './../assets/fonts/VT323-Regular.ttf'
 import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
@@ -9,6 +9,11 @@ export const GlobalStyle = createGlobalStyle`
     padding: 0;
     box-sizing: border-box;
   }
+
+  @font-face {
+      font-family: 'VT323' ;
+      src: url(${VT323});
+    }
 `
 
 export const DivContainerIntro = styled.div`
@@ -17,4 +22,52 @@ export const DivContainerIntro = styled.div`
 
   background-image: url('https://cutewallpaper.org/21/desktop-background-gif/Pokemon-Gold-Desktop-Background-Animation-Album-on-Imgur.gif');
   background-size: cover;
+`
+
+export const Button = styled.button`
+font-family: VT323;
+background-color: #1DA1F2;
+position: absolute;
+right: 45%;
+top: 78%;
+font-size: 24px;
+  width: 150px;
+  height: 50px;
+  background: #06C1DE;
+  border: 0px;
+  box-shadow: 
+    inset -4px 2px 1px 1px grey,
+    inset -4px -2px 1px 1px lightgray,
+    inset 4px 0px 1px 1px lightgray; 
+
+    &:hover{
+      cursor: pointer;
+	background-color: #1DA1F2;
+    }
+    &:active{
+  box-shadow: 
+    -4px 2px 1px 1px grey,
+    -4px -2px 1px 1px lightgray,
+    4px 0px 1px 1px lightgray; 
+    }
+    &:after{
+      content: '';
+  background: black;
+  position: absolute;
+  left: -2.5%;
+  top: 0;
+  width: 105%;
+  height: 100%;
+  z-index: -1;
+    }
+    &:before{
+      content: '';
+  background: black;
+  position: absolute;
+  left: 0;
+  top: -5%;
+  width: 100%;
+  height: 113%;
+  z-index: -1;
+    }
 `
