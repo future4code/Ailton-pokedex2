748b0a6d45c24d3df53e1406dfc26915f3fb27c8
savio henriques mourao ayres <savioayres@hotmail.com>
2022-07-23 10:25:58 -0300

    Não olhar

--- projeto-pokedex/src/Constants/GlobalState.js
+++ projeto-pokedex/src/Constants/GlobalState.js
@@ -1,53 +1,50 @@
-import React, { useContext, useEffect, useState } from "react";
-import { Context } from "./createContext";
-import { GetPokemons } from "../Hooks/useRequestData";
-import { BASE_URL } from "../Constants/Url";
-import axios from "axios";
+import React, { useContext, useEffect, useState } from 'react'
+import { Context } from './createContext'
+import { GetPokemons } from '../Hooks/useRequestData'
+import { BASE_URL } from '../Constants/Url'
+import axios from 'axios'
 
 export default function GlobalState(props) {
-  const Provider = Context.Provider;
-  const [listNamePokemons, setListNamePokemons] = useState([]);
-  const [dataPokemons, setDataPokemons] = useState([]);
-  const [pokedex, setPokedex] = useState([]);
+  const Provider = Context.Provider
+  const [listNamePokemons, setListNamePokemons] = useState([])
+  const [dataPokemons, setDataPokemons] = useState([])
+  const [pokedex, setPokedex] = useState([])
 
   useEffect(() => {
     axios
       .get(`${BASE_URL}`)
       .then((res) => {
-        setListNamePokemons(res.data.results);
+        setListNamePokemons(res.data.results)
       })
       .catch((err) => {
-        console.log(err);
-      });
-  }, []);
-
- 
+        console.log(err)
+      })
+  }, [])
 
   useEffect(() => {
-    const listaPokemons = [];
+    const listaPokemons = []
     listNamePokemons &&
       listNamePokemons.map((pokemon) => {
         axios
           .get(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
           .then((res) => {
-            listaPokemons.push(res.data);
+            listaPokemons.push(res.data)
             if (listaPokemons.length === 20) {
-              setDataPokemons(listaPokemons);
+              setDataPokemons(listaPokemons)
             }
           })
           .catch((err) => {
-            console.log(err);
-          });
-      });
-  }, [listNamePokemons]);
+            console.log(err)
+          })
+      })
+  }, [listNamePokemons])
 
   const values = {
     listNamePokemons,
     dataPokemons,
     pokedex,
-    setPokedex,
-   
-  };
+    setPokedex
+  }
 
-  return <Provider value={values}>{props.children}</Provider>;
+  return <Provider value={values}>{props.children}</Provider>
 }
